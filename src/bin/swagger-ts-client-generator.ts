cb865c67b01ed9dd0e7dd34bf713c207e31dc302
Narazaka <info@narazaka.net>
2017-10-25 15:29:27 +0900

    fix bug

--- src/bin/swagger-ts-client-generator.ts
+++ src/bin/swagger-ts-client-generator.ts
@@ -4,18 +4,20 @@ import { genClient } from "../lib/genClient";
 process.stdin.setEncoding("utf8");
 process.stdin.resume();
 
+let processed = false;
 let stdinData = "";
+
+async function onEnd() {
+    if (processed) return;
+    processed = true;
+    // tslint:disable-next-line no-console
+    console.log(await genClient(stdinData));
+}
 
 process.stdin.on("data", function(chunk) {
     stdinData += chunk;
 });
 
-process.stdin.on("close", async function() {
-    // tslint:disable-next-line no-console
-    console.log(await genClient(stdinData));
-});
+process.stdin.on("close", onEnd);
 
-process.stdin.on("end", async function() {
-    // tslint:disable-next-line no-console
-    console.log(await genClient(stdinData));
-});
+process.stdin.on("end", onEnd);
